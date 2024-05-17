719c7a0fcb07c5711220a4855ac0070e4e76b1f4
jayiuk <jayiuk987@gmail.com>
2024-05-17 14:05:18 +0900

    네이버맵

--- lib/map.dart
+++ lib/map.dart
@@ -1,40 +1,93 @@
+import 'dart:async';
+import 'dart:developer' show log;
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
 
-void main() => runApp(const MyApp());
-
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  _MyAppState createState() => _MyAppState();
+void main() async {
+  await _initialize();
+  runApp(const NaverMapApp());
 }
 
-class _MyAppState extends State<MyApp> {
-  late GoogleMapController mapController;
+Future<void> _initialize() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await NaverMapSdk.instance.initialize(
+      clientId: 'ym730xf02d',
+      onAuthFailed: (ex) => log("********* 네이버맵 인증오류 : $ex *********"));
+}
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+class NaverMapApp extends StatelessWidget {
+  final int? testId;
 
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-  }
+  const NaverMapApp({super.key, this.testId});
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+      home: testId == null
+          ? const FirstPage()
+          : TestPage(key: Key("testPage_$testId")));
+}
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Maps Sample App'),
-          backgroundColor: Colors.green[700],
-        ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
-          ),
-        ),
-      ),
+    return Scaffold(
+        appBar: AppBar(title: const Text('First Page')),
+        body: Center(
+            child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const TestPage()));
+                },
+                child: const Text('Go to Second Page'))));
+  }
+}
+
+class TestPage extends StatefulWidget {
+  const TestPage({Key? key}) : super(key: key);
+
+  @override
+  State<TestPage> createState() => TestPageState();
+}
+
+class TestPageState extends State<TestPage> {
+  late NaverMapController _mapController;
+  final Completer<NaverMapController> mapControllerCompleter = Completer();
+
+  @override
+  Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
+    final pixelRatio = mediaQuery.devicePixelRatio;
+    final mapSize =
+    Size(mediaQuery.size.width - 32, mediaQuery.size.height - 72);
+    final physicalSize =
+    Size(mapSize.width * pixelRatio, mapSize.height * pixelRatio);
+
+    print("physicalSize: $physicalSize");
+
+    return Scaffold(
+      backgroundColor: const Color(0xFF343945),
+      body: Center(
+          child: SizedBox(
+              width: mapSize.width,
+              height: mapSize.height,
+              // color: Colors.greenAccent,
+              child: _naverMapSection())),
     );
   }
+
+  Widget _naverMapSection() => NaverMap(
+    options: const NaverMapViewOptions(
+        indoorEnable: true,
+        locationButtonEnable: false,
+        consumeSymbolTapEvents: false),
+    onMapReady: (controller) async {
+      _mapController = controller;
+      mapControllerCompleter.complete(controller);
+      log("onMapReady", name: "onMapReady");
+    },
+  );
 }
