eb65ce5c59e7a51fc338dcbe73fee722a520600a
jayiuk <jayiuk987@gmail.com>
2024-05-17 15:00:14 +0900

    시작위치

--- lib/map.dart
+++ lib/map.dart
@@ -81,7 +81,7 @@ class TestPageState extends State<TestPage> {
 
   Widget _naverMapSection() => NaverMap(
     options: const NaverMapViewOptions(
-      initialCameraPosition: NCameraPosition(target: NLatLng(126.8255598, 37.4877974), zoom: 10),
+      initialCameraPosition: NCameraPosition(target: NLatLng(37.4877974, 126.8255598), zoom: 10),
         indoorEnable: true,
         locationButtonEnable: false,
         consumeSymbolTapEvents: false),
