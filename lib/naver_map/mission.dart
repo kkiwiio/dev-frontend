ef1a732a75687adcfee6228ab3fcb5c3a82ba85f
Yuna kim <kyaa0903@gmail.com>
2024-05-31 15:44:52 +0900

    splash screen

--- lib/naver_map/mission.dart
+++ lib/naver_map/mission.dart
@@ -1,109 +1,11 @@
-// import 'package:flutter/material.dart';
-// import 'package:project_heck/naver_map/image_picker.dart';
-// import 'package:project_heck/main.dart';
-
-// class MissionDialog extends StatelessWidget {
-//   final String missionDescription;
-//   final String missionImage;
-
-//   const MissionDialog(
-//       {super.key,
-//       required this.missionDescription,
-//       required this.missionImage});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Dialog(
-//       shape: RoundedRectangleBorder(
-//         borderRadius: BorderRadius.circular(30),
-//       ),
-//       child: Padding(
-//         padding: const EdgeInsets.all(16.0),
-//         child: Column(
-//           mainAxisSize: MainAxisSize.min,
-//           crossAxisAlignment: CrossAxisAlignment.center,
-//           children: [
-//             const Center(
-//               child: Text(
-//                 '미션',
-//                 style: TextStyle(
-//                   fontSize: 20,
-//                   fontFamily: 'GmarketSansTTFBold',
-//                 ),
-//                 textAlign: TextAlign.center,
-//               ),
-//             ),
-//             const SizedBox(height: 16),
-//             Center(
-//               child: Text(
-//                 missionDescription,
-//                 style: const TextStyle(
-//                   fontFamily: 'GmarketSansTTFMedium',
-//                   fontSize: 13,
-//                 ),
-//                 textAlign: TextAlign.center,
-//               ),
-//             ),
-//             const SizedBox(height: 16),
-//             Image.asset(
-//               missionImage,
-//               height: 150,
-//               width: double.infinity,
-//               fit: BoxFit.cover,
-//             ),
-//             const SizedBox(height: 15),
-//             Center(
-//               child: SizedBox(
-//                 width: 120,
-//                 height: 35,
-//                 child: TextButton(
-//                   style: TextButton.styleFrom(
-//                     backgroundColor: const Color(0xFF6CBDCA),
-//                     shape: RoundedRectangleBorder(
-//                       borderRadius: BorderRadius.circular(20),
-//                     ),
-//                   ),
-//                   onPressed: () {
-//                     // 카메라 스크린으로 전환
-//                     Navigator.push(
-//                       context,
-//                       MaterialPageRoute(builder: (context) => CameraScreen()),
-//                     );
-//                   },
-//                   child: const Row(
-//                     mainAxisSize: MainAxisSize.min,
-//                     children: [
-//                       Icon(Icons.camera_alt, color: Colors.black),
-//                       SizedBox(width: 8),
-//                       Text(
-//                         "사진찍기",
-//                         style: TextStyle(
-//                           color: Color.fromARGB(255, 0, 0, 0),
-//                           fontFamily: 'GmarketSansTTFBol',
-//                           fontSize: 14,
-//                         ),
-//                       ),
-//                     ],
-//                   ),
-//                 ),
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'display.dart'; // DisplayImageScreen 클래스를 import 합니다.
+import 'display.dart';
 
 class MissionDialog extends StatelessWidget {
   final String missionDescription;
   final String missionImage;
-  final ImagePicker _picker = ImagePicker(); // ImagePicker 인스턴스를 추가합니다.
-
+  final ImagePicker _picker = ImagePicker();
   MissionDialog({
     super.key,
     required this.missionDescription,
@@ -178,7 +80,7 @@ class MissionDialog extends StatelessWidget {
                       borderRadius: BorderRadius.circular(20),
                     ),
                   ),
-                  onPressed: () => _takePicture(context), // 카메라를 직접 호출
+                  onPressed: () => _takePicture(context),
                   child: const Row(
                     mainAxisSize: MainAxisSize.min,
                     children: [
