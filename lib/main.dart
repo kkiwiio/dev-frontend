5a283ddf75bc43dfc6e9dd3ae496c46ef67c1afa
kiwiio <kyaa0903@gmail.com>
2024-10-26 19:01:18 +0900

    fix:eng.ver

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await NaverMapSdk.instance.initialize(
-    clientId: '7vff5ieoeb',
+    clientId: ,
     onAuthFailed: (error) {
       print('Auth failed: $error');
     },
@@ -22,16 +22,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // const 제거됨
       localizationsDelegates: const [
-        // 개별 리스트에는 const 추가
         AppLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
       supportedLocales: const [
-        // 개별 리스트에는 const 추가
         Locale('en', ''), // 영어
         Locale('ko', ''), // 한국어
       ],
